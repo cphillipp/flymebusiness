1ac96d9d2aa31e50bc16fc3102bd7d682ef40c2a
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2013-03-12 14:41:53 -0600

    updated boxffile

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -15,6 +15,18 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+//define('DB_NAME', 'gold');
+
+/** MySQL database username */
+//define('DB_USER', 'root');
+
+/** MySQL database password */
+//define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+//define('DB_HOST', 'localhost');
+
 if (isset($_SERVER['PLATFORM']) && $_SERVER['PLATFORM'] == 'PAGODABOX') {
     define('DB_NAME', $_SERVER['DB1_NAME']);
     define('DB_USER', $_SERVER['DB1_USER']);
